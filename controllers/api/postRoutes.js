8c7334956bb69ee860cce4f84ca558e717dec304
umairkhalid <umairkhalid@fastmail.fm>
2022-05-28 21:38:47 +1000

    eslint correction

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -16,22 +16,22 @@ router.post('/', withAuth, async (req, res) => {
 });
 
 router.put('/:id', withAuth, async (req, res) => {
-    try {
-        const postData = await Post.update(req.body, {
-            where: {
-                id: req.params.id,
-            },
-        });
+  try {
+    const postData = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
 
-        if (!postData[0]) {
-            res.status(404).json({ message: 'No post found with that id!' });
-            return;
-        }
-        res.status(200).json(postData);
-
-    } catch (err) {
-        res.status(500).json(err);
+    if (!postData[0]) {
+      res.status(404).json({ message: 'No post found with that id!' });
+      return;
     }
+    res.status(200).json(postData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', withAuth, async (req, res) => {
