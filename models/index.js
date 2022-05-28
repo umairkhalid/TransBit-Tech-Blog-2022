7723e69eafe81b74e499bc0a207f96bb80c02346
umairkhalid <umairkhalid@fastmail.fm>
2022-05-28 14:04:17 +1000

    Dependencies established and user model fixed

---
+++ models/index.js
@@ -0,0 +1,39 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// Products belongsTo Category
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+// Categories have many Products
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+// Categories have many Products
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'postId'
+});
+
+module.exports = {
+  User,
+  Post,
+  Comment
+};
